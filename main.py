c3bca6f45312bf4848ca027796d216a8758f94b3
TheSoupMasta <78989674+TheSoupMasta@users.noreply.github.com>
2021-07-19 22:10:27 -0400

    Added googling

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+
 import pyaudio
 import wolframalpha
 import PySimpleGUI as sg
 import pyttsx3
 import speech_recognition as sr
+import webbrowser as wb
 client = wolframalpha.Client("no peeking")
 engine = pyttsx3.init()
 
@@ -11,12 +13,13 @@ class Jarvis():
     def __init__(self, mic_obj, rec_obj):
         self.mic = mic_obj
         self.r = rec_obj
+        self.r.energy_threshold = 300
         self.search_engine()
 
 
     def search_engine(self):
         sg.theme('DarkPurple')
-        layout = [[sg.Text('Enter a command'), sg.InputText()], [sg.Button('Ok'), sg.Button('Cancel'), sg.Button('Use Voice Recognition')]]
+        layout = [[sg.Text('Enter a command'), sg.InputText()], [sg.Button('WolfRamAlpha'), sg.Button('Google'), sg.Button('Use Voice Recognition'), sg.Button('Cancel')]]
         self.window = sg.Window('PyDa', layout)
 
         while True:
@@ -26,12 +29,21 @@ class Jarvis():
             if event in (None, 'Use Voice Recognition'):
                 self.voicerecognition()
                 break
-            try:
-                wolfram_res = next(client.query(values[0]).results).text
-                engine.say(wolfram_res)
-                sg.PopupNonBlocking("Wolfram Result: " + wolfram_res)
-            except:
-                quit('Error')
+            if event in (None, 'Google'):
+                try:
+                    wb.get().open_new_tab('https://www.google.com/search?q=' + values[0][0::])
+                except:
+                    print('Error')
+                    self.search_engine()
+            if event in (None, 'Ok'):
+                try:
+                    wolfram_res = next(client.query(values[0]).results).text
+                    engine.say(wolfram_res)
+                    sg.PopupNonBlocking("Wolfram Result: " + wolfram_res)
+                except:
+                    quit('Error')
+
+
 
 
             engine.runAndWait()
@@ -65,20 +77,24 @@ class Jarvis():
                     response = rec_obj.recognize_google(audio)
                 except(sr.UnknownValueError):
                     quit("Could not recognize")
-            try:
-                wolfram_res = next(client.query(response).results).text
-                engine.say(wolfram_res)
-                engine.runAndWait()
-                layout = [[sg.Text(wolfram_res)], [sg.Button('Ok')]]
-                self.popup = sg.Window('Popup', layout)
-                while True:
-                    event, values = self.popup.read()
-                    if event in (sg.WIN_CLOSED, 'Ok'):
-                        self.popup.close()
-                        break
-                self.voicerecognition()
-            except(StopIteration):
-                print("Could not find answer")
+            if response.split()[0] != 'Google':
+                try:
+                    wolfram_res = next(client.query(response).results).text
+                    engine.say(wolfram_res)
+                    engine.runAndWait()
+                    layout = [[sg.Text(wolfram_res)], [sg.Button('Ok')]]
+                    self.popup = sg.Window('Popup', layout)
+                    while True:
+                        event, values = self.popup.read()
+                        if event in (sg.WIN_CLOSED, 'Ok'):
+                            self.popup.close()
+                            break
+                    self.voicerecognition()
+                except(StopIteration):
+                    print("Could not find answer")
+                    self.voicerecognition()
+            elif response.split()[0] == 'Google':
+                wb.get().open_new_tab('https://www.google.com/search?q=' + response[7::])
                 self.voicerecognition()
 
 if __name__ == "__main__":
